1e637a5a9446c7764ff65cb5455a33bcdc4144cd
hzqingze <136786817@qq.com>
2017-04-10 16:35:58 +0800

    更新功能，优化代码

--- src/models/connect.js
+++ src/models/connect.js
@@ -14,7 +14,7 @@ db.once('open', () => {
     console.log("database connected")
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/ysf', function (err) {
+mongoose.connect('mongodb://59.111.99.122:27017/ysf', function (err) {
     if (err) {
         console.error('connect to %s error: ', 'ysf', err.message);
         process.exit(1);
